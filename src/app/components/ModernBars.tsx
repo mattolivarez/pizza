1ee32a28beae30ccb8cafec3e511906a16480a16
Matt Olivarez <mjolivarez1994@gmail.com>
2025-05-17 09:43:40 -0500

    initialized pizza project

---
+++ src/app/components/ModernBars.tsx
@@ -0,0 +1,14 @@
+export default function ModernBars() {
+    return (
+        <svg 
+            xmlns="http://www.w3.org/2000/svg" 
+            className="cursor-pointer absolute top-[18px] sm:top-[16px] md:top-[18px] right-[20px] sm:text-3xl text-xl" 
+            height="1em" 
+            fill="none" 
+            viewBox="0 0 24 24" 
+            stroke="currentColor">
+                <path 
+                    d="M4 6h16M4 12h8m-8 6h16" />
+        </svg>
+    );
+}
